3bcd93b65df12a7328146942e57a6fda11933095
Florian Blaschke <florianblaschke@web.de>
2023-10-16 21:49:24 +0200

    prisma generate

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,7 @@
 export default function NotFound() {
-  return <h1>Shit</h1>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1 className="font-bold text-2xl">Hier gibts nix zu sehen!</h1>
+    </div>
+  );
 }
